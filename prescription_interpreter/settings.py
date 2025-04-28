603ce6170f28ec3842247e8cba4fd7aaf506afd9
Sahil-Gupta628 <sahilguptak6@gmail.com>
2025-04-28 10:47:50 +0530

    Updated

--- prescription_interpreter/settings.py
+++ prescription_interpreter/settings.py
@@ -19,7 +19,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic',
+    # 'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
